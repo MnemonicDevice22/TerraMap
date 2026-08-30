dbea8a512e549ad03bf87ba8ddd22aa0cbefe892
MnemonicDevice22 <164077262+MnemonicDevice22@users.noreply.github.com>
2026-08-30 15:00:03 -0600

    Add files via upload

---
+++ sw(1).js
@@ -0,0 +1,28 @@
+const CACHE = "terramap-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest",
+                "./icon-512.png", "./maskable-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((k) => Promise.all(k.filter((x) => x !== CACHE).map((x) => caches.delete(x))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((hit) => hit || caches.match("./index.html")))
+  );
+});
